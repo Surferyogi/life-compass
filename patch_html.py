825e6411f2b396f141503c53fc3c14d210f50d1f
Sophia <sophiachenyq@gmail.com>
2026-05-19 00:47:56 +0900

    Fix patch_html.py with correct quotes

--- patch_html.py
+++ patch_html.py
@@ -1,6 +1,5 @@
-
 c = open("dist/index.html").read()
-c = c.replace("src="/_expo/", "src="/life-compass/_expo/")
-c = c.replace("href="/favicon", "href="/life-compass/favicon")
-open("dist/index.html","w").write(c)
-print("Patched")
+c = c.replace("src=\"/_expo/", "src=\"/life-compass/_expo/")
+c = c.replace("href=\"/favicon", "href=\"/life-compass/favicon")
+open("dist/index.html", "w").write(c)
+print("Patched:", c[c.find("script"):c.find("script")+80])
